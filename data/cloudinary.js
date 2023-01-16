0729a4b3f2db005b104035805afc13f58ffbc0b4
abrahem elnakeeb <abrahem.elnakeeb@stu.shenkar.ac.il>
2023-01-16 21:57:45 +0200

    initialization cloudinary api

--- data/cloudinary.js
+++ data/cloudinary.js
@@ -3,7 +3,9 @@ const cloudinary = require("cloudinary");
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
     api_key:    process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true
 });
+
 
 module.exports = { cloudinary };
