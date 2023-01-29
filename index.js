39cf3ed3c9c5ef4e0012071166db2bd318877928
abrahem elnakeeb <abrahem.elnakeeb@stu.shenkar.ac.il>
2023-01-29 17:12:04 +0200

    The function of connecting with the data base and increasing the size of the received messages

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require('body-parser');
 const sessions = require("express-session");
 const cookieParser = require("cookie-parser");
 const  { userRouter }  = require("./routers/userRouter");
 const  { sessionRouter }  = require("./routers/sessionRouter");
 const { pageRouter } = require("./routers/pageRouter");
+const { reportRouter } = require("./routers/reportRouter");
+const { connect } = require("./data/DBconnection");
+
 
 // Configurations //
 const PORT = process.env.PORT || 8000;
+connect();
 
 const app = express();
 
@@ -16,8 +21,8 @@ app.set("views", "./views");
 app.set("view engine", "ejs");
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: false, parameterLimit: 50000 }));
 
 app.use(express.static("public"));
 app.use("/css", express.static(__dirname + "public/css"));
@@ -36,9 +41,12 @@ app.use(sessions({
     cookie: { maxAge: Day }
 }));
 
-app.use("/users", userRouter);
+
+
 app.use("/session", sessionRouter);
-app.use("/accessibility", pageRouter);
+app.use("/users", userRouter);
+app.use("/reports", reportRouter);
+app.use("/", pageRouter);
 
 app.use((req, res) => {
     res.status(400).send("Something is broken!");
