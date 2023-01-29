0f2ca7a0a52576f988b298f948b40385c056cb30
abrahem elnakeeb <abrahem.elnakeeb@stu.shenkar.ac.il>
2023-01-29 17:23:15 +0200

    profile image uploading to the cloud

--- repositories/usersRepository.js
+++ repositories/usersRepository.js
@@ -1,6 +1,25 @@
+const bcrypt = require("bcrypt");
 const Storage = require("../data/storage");
-const { cloudinary } = require("../data/cloudinary");
-const moment = require("moment");
+const Logger  = require("../logger/Logger");
+const { Image } = require("../models/includesModels");
+const {cloudinary} = require("../data/cloudinary");
+
+const logger = new Logger();
+
+const deleteImage = async function (publicId) {
+    if (publicId === "default")
+        return;
+    try {
+        await cloudinary.v2.uploader.destroy(publicId);
+    } catch (err) {
+        logger.log(err);
+    }
+}
+
+async function hashPassword(plaintextPassword) {
+    return await bcrypt.hash(plaintextPassword, 10);
+}
+
 
 module.exports = class UsersRepository {
     constructor() {
@@ -11,26 +30,66 @@ module.exports = class UsersRepository {
         return this.storage.find();
     }
 
-    findUser(email, pass) {
-        return pass ? this.storage.findItem({email: email}).select("+password") : this.storage.findItem({email: email});
+    findUser(id, pass) {
+        return pass ? this.storage.findItem({_id: id}).select("+password") : this.storage.findItem({_id: id});
     }
 
-    addUser(user) {
-        user["createdAt"] = moment().format("MM-DD-YY hh:mm");
-        user["updatedAt"] = moment().format("MM-DD-YY hh:mm");
-        console.log(user);
-        return this.storage.addItem(user);
+    async updateImage(id, img, publicID) {
+        let image;
+        if ( img === "delete") {
+            const res = await deleteImage(publicID);
+            image = new Image();
+        }
+        else if (img !== undefined){
+            const res = await deleteImage(publicID);
+            const uploadedResponse = await cloudinary.v2.uploader.upload(img, {
+                upload_preset: "profile-preset"
+            });
+            image = new Image({
+                publicId: uploadedResponse.public_id,
+                secureUrl: uploadedResponse.secure_url
+            });
+        }
+        return this.storage.updateItem({_id: id}, { image: image});
     }
 
-    updateUser(email, user) {
-        user["updatedAt"] = moment().format("MM-DD-YY hh:mm");
-        if(user.hasOwnProperty("createdAt"))
+    async addUser(user) {
+
+        const imgFile  = user.hasOwnProperty("imgFile") ? user.imgFile : undefined;
+        if(imgFile !== undefined)
+            delete user.imgFile;
+
+        user["password"] = await hashPassword(user["password"]);
+        user["updatedAt"] = new Date();
+        const { _id } = await this.storage.addItem(user);
+        return await this.updateImage(_id, imgFile, undefined);
+    }
+
+
+
+    async updateUser(id, old, user) {
+        user["updatedAt"] = new Date();
+
+        const imgFile  = user.hasOwnProperty("imgFile") ? user.imgFile : undefined;
+        if(imgFile !== undefined)
+            delete user.imgFile;
+
+        if (user.hasOwnProperty("createdAt"))
             delete user.createdAt;
-        return this.storage.updateItem({ email: email}, user);
+
+        await this.storage.updateItem({_id: id}, user);
+        await this.updateImage(id, imgFile, old.image.publicId);
+
     }
 
-    deleteUser(email) {
-        return this.storage.deleteItems({ email: { $in: email}});
+    async deleteUser(id, image) {
+        if (image.publicId !== "publicId")
+            await deleteImage(image.publicId);
+        return this.storage.deleteItem({_id: id});
+    }
+
+    filter(criterias) {
+        return this.storage.findItem(criterias);
     }
 
 };
