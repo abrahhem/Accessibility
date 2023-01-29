a5a7f094ac77044fabedb0f78b44afab0d3ac3ea
abrahem elnakeeb <abrahem.elnakeeb@stu.shenkar.ac.il>
2023-01-29 17:21:35 +0200

    Adding paths to new pages

--- controllers/pagesController.js
+++ controllers/pagesController.js
@@ -1,24 +1,146 @@
+const UsersRepository   = require("../repositories/usersRepository");
+const ReportsRepository = require("../repositories/reportsRepository");
+const reportsRepository = new ReportsRepository();
+const usersRepository   = new UsersRepository();
+
+
+
+async function findUser(userID) {
+    const user = await usersRepository.findUser(userID, false);
+    if (user.length !== 1)
+        return false;
+    return user[0];
+}
+
+async function findReport(reportID) {
+
+    const report = await reportsRepository.findReport(reportID);
+    if (report.length !== 1)
+        return false;
+
+    const user = await usersRepository.findUser(report[0].postedBy, false);
+    if (user.length !== 1)
+        return false;
+
+    return {
+        user: user[0],
+        report: report[0]
+    };
+
+}
+
+async function userReports(userID) {
+
+    const user = await usersRepository.findUser(userID, false);
+    if (user.length !== 1)
+        return false;
+    const reports = await reportsRepository.findReportsByUser(userID);
+
+    return {
+        user: user[0],
+        reports: reports
+    };
+
+}
+
+async function all() {
+    const users = await usersRepository.find();
+    const reports = await reportsRepository.find();
+    const data = [];
+    reports.forEach(report => {
+
+        const user = users.filter(user => user._id.equals(report.postedBy));
+
+        data.push( {
+            user: user[0],
+            report: report
+        });
+    });
+    return data;
+}
+
+
+
 
 exports.pagesController = {
 
     getIndex(req, res) {
-        res.render("index");
+        if(req.session.hasOwnProperty("userInfo"))
+            res.redirect("home");
+        else
+            res.render("index");
     },
 
-    getHome(req, res) {
-        if(!req.session.hasOwnProperty("userInfo"))
-            res.render("index");
-        else
-            res.render("home");
+    async getHome(req, res) {
+        if (!req.session.hasOwnProperty("userInfo"))
+            res.redirect("/");
+        else {
+            const data = await all();
+            res.render("home", {user: req.session.userInfo, data: data});
+        }
     },
 
     getMap(req, res) {
         if(!req.session.hasOwnProperty("userInfo"))
-            res.render("index");
+            res.redirect("/");
         else
-            res.render("map");
+            res.render("map", { user: req.session.userInfo });
     },
     getError(req, res) {
         res.render("404");
+    },
+    async getUsers(req, res) {
+        if (!req.session.hasOwnProperty("userInfo"))
+            res.redirect("/");
+        else {
+            const users = await usersRepository.find();
+            res.render("usersList", {user: req.session.userInfo, users: users});
+        }
+    },
+    getReportForm(req, res) {
+        if(!req.session.hasOwnProperty("userInfo"))
+            res.redirect("/");
+        else
+            res.render("reportForm", { user: req.session.userInfo });
+    },
+
+    async getProfile(req, res) {
+        if (!req.session.hasOwnProperty("userInfo"))
+            res.redirect("/");
+        else {
+            const { id } = req.params;
+            if (req.session.userInfo._id !== id) {
+                try {
+                    const user = await findUser(id);
+
+                    if (user === false)
+                        res.render("404");
+                    else
+                        res.render("profile", {user: req.session.userInfo, info: user});
+                } catch (e) {
+                    res.render("404");
+                }
+            } else {
+                res.render("profile", {user: req.session.userInfo, info: req.session.userInfo});
+            }
+        }
+    },
+
+    async getReport(req, res) {
+        if (!req.session.hasOwnProperty("userInfo"))
+            res.redirect("/");
+        else {
+            const {id} = req.params;
+            try {
+                const report = await findReport(id);
+                if (report === false)
+                    res.render("404");
+                else
+                    res.render("report", {user: req.session.userInfo, data: report});
+            } catch (e) {
+                res.render("404");
+            }
+        }
     }
+
 }
--- routers/pageRouter.js
+++ routers/pageRouter.js
@@ -4,9 +4,14 @@ const { pagesController } = require('../controllers/pagesController');
 const pageRouter = new Router();
 
 
-pageRouter.get("/index", pagesController.getIndex);
+pageRouter.get("/", pagesController.getIndex);
 pageRouter.get("/home", pagesController.getHome);
+pageRouter.get("/reportForm", pagesController.getReportForm);
 pageRouter.get("/map", pagesController.getMap);
+pageRouter.get("/usersList", pagesController.getUsers);
+pageRouter.get("/profile/:id", pagesController.getProfile);
+pageRouter.get("/report/:id", pagesController.getReport);
+pageRouter.get("/reports", pagesController.getReport);
 pageRouter.get("/*", pagesController.getError);
 
 
